86a9548f91387ac6c0024923a33f3683f4ecb83a
George Solorio <georgemsolorio@gmail.com>
2020-06-27 19:41:12 -0700

    Initial Commit

---
+++ appStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appStoreClone
+//
+//  Created by George Solorio on 6/27/20.
+//  Copyright © 2020 George Solorio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
