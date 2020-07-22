6a2bc011315796a81c680060de8cd01f757f6dcb
George Solorio <georgemsolorio@gmail.com>
2020-07-21 20:43:52 -0700

    Removed comment

--- appStoreClone/Views/Today/TodayAppFullScreenHeaderCell.swift
+++ appStoreClone/Views/Today/TodayAppFullScreenHeaderCell.swift
@@ -18,11 +18,6 @@ class TodayAppFullScreenHeaderCell: UITableViewCell {
         
         addSubview(todayCell)
         todayCell.fillSuperView()
-
-//        addSubview(closeButton)
-//        closeButton.anchor(top: topAnchor, leading: nil, bottom: nil, trailing: trailingAnchor,
-//                           padding: .init(top: 44, left: 0, bottom: 0, right: 12),
-//                           size: .init(width: 80, height: 38))
     }
     
     required init?(coder: NSCoder) {
