1034257a380786cf476bc16e5fa265b91b6fb75e
George Solorio <georgemsolorio@gmail.com>
2020-08-01 20:16:48 -0700

    Improved compositional layout

--- appStoreClone/Controller/Apps/CompositionalGroup/AppsCompositionalView.swift
+++ appStoreClone/Controller/Apps/CompositionalGroup/AppsCompositionalView.swift
@@ -21,7 +21,20 @@ class ComposionalController: UICollectionViewController {
     }
     
     init() {
-        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+        
+        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+        
+        item.contentInsets.bottom = 16
+        item.contentInsets.trailing = 16
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: .init(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(300)), subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        section.contentInsets.leading = 32
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        super.init(collectionViewLayout: layout)
     }
     
     required init?(coder: NSCoder) {
@@ -29,7 +42,7 @@ class ComposionalController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 8
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
